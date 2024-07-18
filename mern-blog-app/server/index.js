f2f11f83df8bfa9449461749ba9a86170e7145f7
Karthik <karthik2502@protonmail.com>
2024-07-18 19:50:33 +0530

    testing database connection - successful

--- mern-blog-app/server/index.js
+++ mern-blog-app/server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const blogRouter = require("./route/blog-route");
-
+require("./db");
 const app = express();
 app.use(cors());
 app.use(express.json());
