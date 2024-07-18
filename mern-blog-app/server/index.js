dd2de642fcc12d49f24fa30f3ada21bfa4ed4693
Karthik <karthik2502@protonmail.com>
2024-07-18 19:48:48 +0530

    Adding a new mongodb database connection

--- mern-blog-app/server/index.js
+++ mern-blog-app/server/index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const blogRouter = require("./route/blog-route");
 
-require("./db");
-
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -11,7 +9,7 @@ app.use(express.json());
 app.use("/api/blogs", blogRouter);
 
 app.use("/api", (req, res) => {
-  res.status(200).json({message : "Hello World"});
+  res.status(200).json({ message: "Hello World" });
 });
 
 app.listen(5000, () => console.log(`App is running at 5000...`));
